78b329272498a197330e421eb64cbbcb85b5df03
Maryam23129 <maryam.maryam@asdp.id>
2025-05-23 11:30:48 +0700

    Update financial_metrics.py

--- module/financial_metrics.py
+++ module/financial_metrics.py
@@ -3,9 +3,8 @@ import pandas as pd
 
 def display_financials():
     st.header("Kinerja Keuangan")
-    data = {
+    df = pd.DataFrame({
         "Item": ["Pendapatan", "EBITDA", "Fixed Cost", "Laba Bersih", "Debt"],
         "Nilai (Miliar IDR)": [1500, 450, 300, 200, 1000]
-    }
-    df = pd.DataFrame(data)
+    })
     st.table(df)
